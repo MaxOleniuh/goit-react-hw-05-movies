3c83245daf0de70bebc9d07cdb99b11e5545efb2
oleniuh <maxim.oleniukh@gmail.com>
2023-05-10 10:51:59 +0100

    Header and Loader components created

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,11 +1,15 @@
 import { fetchMoviesApi } from 'services/moviesApi';
-import { HomePage } from 'pages/HomePage';
+import { HomePage } from 'components/HomePage/HomePage';
+import { Header } from 'components/Header/Header';
 import { useEffect, useState } from 'react';
+import { Loader } from 'components/Loader/Loader';
 
 export const App = () => {
   const [movies, setMovies] = useState([]);
+  const [loader, setLoader] = useState(false);
   
   const getMovies = async () => {
+    setLoader(true);
     try {
       const data = await fetchMoviesApi();
       setMovies([...movies, ...data]);
@@ -14,12 +18,17 @@ export const App = () => {
     catch (error) {
       console.log(error);
     }
+    finally {
+      setLoader(false);
+    }
   };
   useEffect(() => {
     getMovies();
   }, [])
   return (
     <>
+      <Header />
+      {loader && <Loader/>}
       <HomePage movies={movies} />
     </>
   );
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+export const Header = () => {
+    return (
+        <header>
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/">Movies</a>
+                </li>
+            </ul>
+        </header>
+    )
+}
